a4411bb15128a045f6fd2547986eff89eea1e64a
cwdatlas <ascott@carroll.edu>
2024-04-05 11:03:19 -0600

    added comments and header

--- src/Baton.java
+++ src/Baton.java
@@ -1,26 +1,39 @@
 package org.player;
 
+/**
+ * @author Aidan Scott
+ * Baton is the 'flip flop mutex' (I know it's not exactly a mutex, but I like the name)
+ * used to make the chorister wait when other notes are being played
+ * and the conductor 'tone' wait when the note is being played
+ */
 public class Baton {
+    // Used to tell which side is waiting.
     private boolean notePlaying = false;
-
-    // ONLY the conductor (Tone) should call this method
-    public synchronized void play(){
+    /**
+     * ONLY the conductor (Tone) should call the play method
+     * This will notify the chorister and make the conductor 'tone' wait for the chorister to finish playing its note
+     */
+    public synchronized void play() {
         // Notifies the waiting chorister to play the note
-        notePlaying = true;
-        this.notify();
-        while(notePlaying){
-            try{
+        notePlaying = true; // set that the note is playing
+        this.notify(); // notify the chorister
+        while (notePlaying) { // make sure if the thread is interrupted, the conductor wont stop waiting
+            try {
                 wait();
             } catch (InterruptedException e) {
+                // Simply notify that the thread was interrupted.
                 System.out.println("Play wait was interrupted");
             }
         }
     }
-    // ONLY Chorister should call this method
+    /**
+     * ONLY Chorister should call the finished method
+     * This will notify the conductor and make the chorister wait for the conductor to say to play another note
+     */
     public synchronized void finished(){
         // Notifies the waiting Baton to play the note
-        notePlaying = false;
-        this.notify();
+        notePlaying = false; // sets notePlaying to false, its finished playing the note, so it shouldn't be true!
+        this.notify(); // tell the conductor to stop waiting (check previous function for more details, it's the same.
         while(!notePlaying){
             try{
                 wait();
@@ -29,7 +42,10 @@ public class Baton {
             }
         }
     }
-    // ONLY used when stopping all choristers and exiting program and playing = false
+    /**
+     * ONLY used when stopping the chorister and exiting program and playing = false
+     * Flush is used to stop the chorister waiting, but doesn't make the conductor 'tone' wait
+     */
     public synchronized void flush(){
         notePlaying = true;
         this.notify();
